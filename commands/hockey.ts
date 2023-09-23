adb3009af569a3455297dce9649e46df76695273
Colin Peterson <colin9416@gmail.com>
2023-09-23 01:52:15 +0000

    Updated hockey command to have choices. Committing before updating command deployment changes.

--- commands/hockey.ts
+++ commands/hockey.ts
@@ -1,61 +1,83 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { BaseCommandInteraction } from "discord.js";
-import getGames from "./getHockey";
+import getGames from "../helpers/getHockey";
 import embedScore from "../helpers/hockeyEmbedder";
 import { Game, Root } from "../data/game";
+import { Boxscore } from "../data/_interfaces";
+import hockeyEmbedder from "../helpers/hockeyEmbedder";
 
 const GAME_LIMIT = 3;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('leafs')
-        .setDescription('Replies with Leafs game stat'),
+        .setDescription('Replies with Leafs game stat')
+        // .addUserOption(option =>
+		// 	option
+		// 		.setName('Team')
+		// 		.setDescription('Team Abbreviation: i.e. "TOR" ')
+		// 		.setRequired(true))
+		.addStringOption((option) =>
+			option.setName('team')
+				.setDescription('Team abbreviation i.e. "TOR  SEA')
+                .setRequired(true)
+                .addChoices(
+					{ name: 'Leafs', value: 'TOR' },
+					{ name: 'Kraken', value: 'SEA' },
+				)
+        ),
     async execute(interaction: BaseCommandInteraction) {
         // const test_game = "2020020001";
-
+        const team = interaction.options.get('team');
         await interaction.deferReply();
 
-        const resp = await getGames() as Root;
+        const score = await getGames() as Boxscore;
 
+        console.log("============ resp: ", score);
 
-
-        if (resp.dates !== undefined && resp.dates.length > 0) {
+        if (score.id > 0) { // TODO: 
 
             let embeds = [];
 
-            console.log('resp: ', resp);
+            // console.log('resp: ', resp);
 
-            let gameDates = resp.dates;
 
-            for (const [i, day] of gameDates.entries()) {
+            let embed = hockeyEmbedder(score);
+
+            embeds.push(embed);
+            // let gameDates = resp.dates;
+
+            // for (const [i, day] of gameDates.entries()) {
                 
-                if (i >= GAME_LIMIT) break;
+            //     if (i >= GAME_LIMIT) break;
                 
 
-                if (day.totalGames > 0) {
+            //     if (day.totalGames > 0) {
 
                 
 
 
 
-                console.log("destructuring root into game day: ", day);
+            //     console.log("destructuring root into game day: ", day);
 
-                let embed = embedScore(day.games[0]);
+            //     let embed = embedScore(day.games[0]);
 
-                embeds.push(embed);
+            //     embeds.push(embed);
 
-                }
+            //     }
 
 
-                // let resp = embedWeather(weather);
-                // await interaction.reply({ embeds: [resp] });
+            //     // let resp = embedWeather(weather);
+            //     // await interaction.reply({ embeds: [resp] });
 
-                // await interaction.editReply(`leafs win woo:ubbyvy ${resp.dates[0].date}`);
+            //     // await interaction.editReply(`leafs win woo:ubbyvy ${resp.dates[0].date}`);
 
-            }
-console.log('embeds ========= : ', embeds);
+            // }
+// console.log('embeds ========= : ', embeds);
 
-            await interaction.editReply({ embeds })
+            // await interaction.editReply({ embeds })
+
+            await interaction.editReply({embeds})
         
         } else {
             await interaction.editReply("No games found.");
