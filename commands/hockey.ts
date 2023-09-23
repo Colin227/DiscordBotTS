81c9a984608796ac2a5f5e6e9968cb56eecd7235
Colin Peterson <colin9416@gmail.com>
2023-09-23 03:57:38 +0000

    Cleaned up old code

--- commands/hockey.ts
+++ commands/hockey.ts
@@ -13,83 +13,31 @@ const Teams = TEAMS;
 
 const teamChoices = Teams.data.slice(0, 25).map((
     team) => {
-        return {
-            name: team.fullName,
-            value: team.triCode
-        }
+    return {
+        name: team.fullName,
+        value: team.triCode
     }
-    );
+}
+);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('hockey')
         .setDescription('Replies with hockey game schedule.')
-        // .addUserOption(option =>
-		// 	option
-		// 		.setName('Team')
-		// 		.setDescription('Team Abbreviation: i.e. "TOR" ')
-		// 		.setRequired(true))
-		.addUserOption((option) =>
-			option.setName('team')
-				.setDescription('Team abbreviation i.e. "TOR  SEA')
+        .addUserOption((option) =>
+            option.setName('team')
+                .setDescription('Team abbreviation i.e. "TOR  SEA')
                 .setRequired(true)
         ),
     async execute(interaction: CommandInteraction) {
-        // const test_game = "2020020001";
         const team = interaction.options.getString('team');
         console.log("team: ", team)
         await interaction.deferReply();
-
-        const score = await getGames(team.trim().toLowerCase()) as GameSchedule;
-
-        console.log("============ resp: ", score);
-
-        if (score) { // TODO: 
-
-            let embeds = [];
-
-            // console.log('resp: ', resp);
-
-
-            let embed = hockeyEmbedder(score);
-
-            embeds.push(embed);
-            // let gameDates = resp.dates;
-
-            // for (const [i, day] of gameDates.entries()) {
-                
-            //     if (i >= GAME_LIMIT) break;
-                
-
-            //     if (day.totalGames > 0) {
-
-                
-
-
-
-            //     console.log("destructuring root into game day: ", day);
-
-            //     let embed = embedScore(day.games[0]);
-
-            //     embeds.push(embed);
-
-            //     }
-
-
-            //     // let resp = embedWeather(weather);
-            //     // await interaction.reply({ embeds: [resp] });
-
-            //     // await interaction.editReply(`leafs win woo:ubbyvy ${resp.dates[0].date}`);
-
-            // }
-// console.log('embeds ========= : ', embeds);
-
-            // await interaction.editReply({ embeds })
-
-            await interaction.editReply({embeds})
-        
-        } else {
-            await interaction.editReply("No games found.");
+        try {
+            const score = await getGames(team.trim().toLowerCase()) as GameSchedule;
+            await interaction.editReply({ embeds: [hockeyEmbedder(score)] })
+        } catch (e) {
+            await interaction.editReply("Error getting game data, try again later.");
         }
     }
 }
