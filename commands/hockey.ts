c523505f8686a0ec0c101b3b9df2c4ebee18a32c
Colin Peterson <colin9416@gmail.com>
2023-09-30 13:58:25 +0000

    Team logos.

--- commands/hockey.ts
+++ commands/hockey.ts
@@ -25,9 +25,9 @@ module.exports = {
             // Get the current game schedule
             // TODO: Unsure how the NHL api will handle this once preseason is over. Does the api return every game in the season? 
             const score = await getGames(t.triCode) as GameSchedule;
-            // Get the team logo from stored assets. These aren't stored on git due to the logos being copyright of their respective teams.
+            // Get the team logo from stored assets.
             const teamFile = new MessageAttachment(`../Discordjs/assets/teams/${t.triCode}_light.png`);
-            
+
             await interaction.editReply({ embeds: [hockeyEmbedder(score, t.triCode)], files: [teamFile] })
         } catch (e) {
             await interaction.editReply("Error getting game data, try again later.");
