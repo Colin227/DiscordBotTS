fdf4dde60a2cf0529817d06f7e32778ca50b60ff
Colin Peterson <colin9416@gmail.com>
2024-01-11 23:59:18 +0000

    NASA photo of the day

--- helpers/hockeyEmbedder.ts
+++ helpers/hockeyEmbedder.ts
@@ -1,14 +1,14 @@
-import { ColorResolvable, MessageEmbed } from "discord.js";
-import { GameSchedule, Team } from "../data/_interfaces";
-import dayjs from "dayjs";
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
+// import { ColorResolvable, MessageEmbed } from "discord.js";
+// import { GameSchedule, Team } from "../data/_interfaces";
+// import dayjs from "dayjs";
+// import utc from 'dayjs/plugin/utc';
+// import timezone from 'dayjs/plugin/timezone';
 
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
+// dayjs.extend(utc);
+// dayjs.extend(timezone);
 
-const tz = "America/Toronto";
+// const tz = "America/Toronto";
 
 // Values for game states, used to determine if a score should be shown.
 export const gameStates = [
@@ -18,39 +18,39 @@ export const gameStates = [
   'OFF'
 ]
 
-export default function embedScore(sched: GameSchedule, team: Team): MessageEmbed {
-  const { 
-    gamesByDate,
-} = sched;
+// export default function embedScore(sched: GameSchedule, team: Team): MessageEmbed {
+//   const { 
+//     gamesByDate,
+// } = sched;
 
-const fields = [];
-for (const [i, game] of gamesByDate.entries()) {
-  const g = game.games[0];
-  const gameDay = dayjs(g.startTimeUTC);
-  const gameLocal = gameDay.tz(tz);
+// const fields = [];
+// for (const [i, game] of gamesByDate.entries()) {
+//   const g = game.games[0];
+//   const gameDay = dayjs(g.startTimeUTC);
+//   const gameLocal = gameDay.tz(tz);
 
-  let gameValue = "";
+//   let gameValue = "";
 
-  const date = gameLocal.format('MM/DD h:mmA')
+//   const date = gameLocal.format('MM/DD h:mmA')
 
-  if (gameStates.includes(g.gameState)) {
-    gameValue = `${g.awayTeam.score} - ${g.homeTeam.score}`;
-  } else {
-    gameValue = date;
-  }
+//   if (gameStates.includes(g.gameState)) {
+//     gameValue = `${g.awayTeam.score} - ${g.homeTeam.score}`;
+//   } else {
+//     gameValue = date;
+//   }
 
-  fields.push({name: `${g.awayTeam.abbrev} @ ${g.homeTeam.abbrev}`, value: `${gameValue}`, inline: true})
+//   fields.push({name: `${g.awayTeam.abbrev} @ ${g.homeTeam.abbrev}`, value: `${gameValue}`, inline: true})
   
-  // Add spacing after every 3 inline fields. Second condition ensures no extra spacing at end.
-  if (fields.filter(field => field.inline).length % 3 === 0 && i+1 !== gamesByDate.length ) {
-    fields.push({ name: '\u200B', value: '\u200B', inline: false });
-  }
-}
+//   // Add spacing after every 3 inline fields. Second condition ensures no extra spacing at end.
+//   if (fields.filter(field => field.inline).length % 3 === 0 && i+1 !== gamesByDate.length ) {
+//     fields.push({ name: '\u200B', value: '\u200B', inline: false });
+//   }
+// }
 
-    const embed = new MessageEmbed()
-    .setColor(team.color as ColorResolvable) // TODO: Check this casting type
-    .setTitle(`${team.triCode} Schedule`)
-    .setThumbnail(`attachment://${team.triCode}_light.png`)
-    .addFields(fields)
-    return embed;
-}
+//     const embed = new MessageEmbed()
+//     .setColor(team.color as ColorResolvable) // TODO: Check this casting type
+//     .setTitle(`${team.triCode} Schedule`)
+//     .setThumbnail(`attachment://${team.triCode}_light.png`)
+//     .addFields(fields)
+//     return embed;
+// }
